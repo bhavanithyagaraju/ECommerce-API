f792405e184ed06d310535b16d5299be76e806c8
Bhavanithyagaraju <bhavaniveena779@gmail.com>
2026-02-28 01:23:35 +0530

    error fixed to build project successfully

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// Add DbContext
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//// Add DbContext
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 builder.Services.AddControllers();
